4743e688dc406efde35252378dcf68d96c924071
Frank Lan <franklan118@gmail.com>
2018-09-22 13:21:06 +1000

    make it general proxy

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors_proxy = require('cors-anywhere')
 cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
+    requireHeader: [],
+    removeHeaders: [],
   })
   .listen(port, host, function () {
     console.info('Running CORS Anywhere on ' + host + ':' + port)
